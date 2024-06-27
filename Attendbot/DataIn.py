dfc5834d6a82b7c7bbe4e1e787bf3689a46e4c0d
Rudra <khan.rudradip@gmail.com>
2024-06-27 23:42:50 +0530

    fixed bribe.manipulate

--- Attendbot/DataIn.py
+++ Attendbot/DataIn.py
@@ -10,12 +10,13 @@ class Bribe():
     
     def manipulate(self,df:pd.DataFrame):
         try:
+            df.fillna("NoValue",inplace=True)
             for i,j in df.iterrows():
-                if (j["Rostered shift"] in ["PH","WO"] ) | (j["Attendance shift"] == "Leave"):
+                if (j["Rostered shift"] in ["PH","WO"] ) | (j["Attended shift"] == "Leave"):
                     df.loc[i,"Mode"] = "ignore"
                 elif j["Attendance status"] == "PendingApproval":
                     df.loc[i,"Mode"] = "ignore"
-                elif not pd.isnull(j["Date"]) and datetime.datetime.strptime(j["Date"],"%d-%b-%Y") >= datetime.datetime.now():
+                elif not pd.isnull(j["Date"]) and datetime.datetime.strptime(j["Date"],"%d-%b-%y").month > datetime.datetime.now().month:
                 #type(df.loc[i,"Date"])!= float:
                     # if datetime.datetime.strptime(j["Date"],"%d-%b-%Y") >= datetime.datetime.now():
                     df.loc[i,"Mode"] = "ignore"
@@ -23,7 +24,7 @@ class Bribe():
                     in_time = j["In Time"]
                     out_time = j["Out Time"]
 
-                    if pd.isnull(in_time) and pd.isnull(out_time):
+                    if in_time == "NoValue" and out_time== "NoValue":
                         pass
                     else:
                         diff = datetime.datetime.strptime(out_time,"%H:%M") - datetime.datetime.strptime(in_time,"%H:%M")
@@ -33,12 +34,14 @@ class Bribe():
                             df.loc[i,"Mode"] = "Swipe Adjustment"
                         else:
                             df.loc[i,"Mode"] = "ignore"
-                df.fillna("NoValue",inplace=True)
+            
             return df
         except Exception as e:
             return str(e)
 
-# df = pd.read_excel("5-attendence-data.xlsx")
+# df = pd.read_csv("sample.csv")
 # br = Bribe()
-# br.manipulate(df)
+# c = br.manipulate(df)
+# print(c)
+
 
